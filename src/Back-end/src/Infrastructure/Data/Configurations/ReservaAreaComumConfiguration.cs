41c1ba76ad9f62a08e063edaae4f0e3bc9c9d5bc
LeandroAugustoSantos <143044412+LeandroAugustoSantos@users.noreply.github.com>
2026-04-09 23:01:41 -0300

    Rename AreaComumConfiguration to ReservaAreaComumConfiguration

---
+++ src/Back-end/src/Infrastructure/Data/Configurations/ReservaAreaComumConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartSindico.Domain.Entities;
+
+namespace SmartSindico.Infrastructure.Data.Configurations;
+
+public class ReservaAreaComumConfiguration : IEntityTypeConfiguration<ReservaAreaComum>
+{
+    public void Configure(EntityTypeBuilder<ReservaAreaComum> builder)
+    {
+        builder.ToTable("ReservaAreaComum");
+        builder.HasKey(ReservaAreaComum => ReservaAreaComum.Id);
+        builder.Property(ReservaAreaComum => a.ReservaAreaComum).
+        IsRequired().
+        HasMaxLength(20);
+    }
+}

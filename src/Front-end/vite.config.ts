ca3cf6ee91bbad1163615d16497297dcc57770ed
LeandroAugustoSantos <143044412+LeandroAugustoSantos@users.noreply.github.com>
2026-06-07 11:02:51 -0300

    Update vite.config.ts

--- src/Front-end/vite.config.ts
+++ src/Front-end/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vitest/config'
 
 const apiTarget = process.env.VITE_API_TARGET ?? 'https://localhost:7107'
+const apiTarget = process.env.VITE_API_TARGET ?? 'http://localhost:5053'
 
 // https://vite.dev/config/
 export default defineConfig({
